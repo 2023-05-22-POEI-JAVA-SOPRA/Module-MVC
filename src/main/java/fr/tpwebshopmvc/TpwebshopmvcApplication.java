d3142236f5fb2e145ec5f0834923c72f7ae0dc92
Matthieu DAVID <mat.david39700@gmail.com>
2023-08-21 11:06:44 +0200

    Implémentation service + repository role + article; création persistance.xml

--- src/main/java/fr/tpwebshopmvc/TpwebshopmvcApplication.java
+++ src/main/java/fr/tpwebshopmvc/TpwebshopmvcApplication.java
@@ -2,8 +2,12 @@ package fr.tpwebshopmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan({"fr.tpwebshopmvc.service","fr.tpwebshopmvc.repository","fr.tpwebshopmvc.model"})
 public class TpwebshopmvcApplication {
 
 	public static void main(String[] args) {
--- src/main/java/fr/tpwebshopmvc/service/ArticleService.java
+++ src/main/java/fr/tpwebshopmvc/service/ArticleService.java
@@ -1,8 +1,34 @@
 package fr.tpwebshopmvc.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import fr.tpwebshopmvc.model.Article;
+import fr.tpwebshopmvc.repository.ArticleRepository;
 
 @Service
 public class ArticleService {
-
+	
+	@Autowired
+	private ArticleRepository repository;
+	
+	
+	public List<Article> findAll()
+	{
+		return (List<Article>) repository.findAll();
+	}
+	
+	public Article findById(Integer id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+	public Article save(Article article) {
+		return repository.save(article);
+	}
+	
+	public void delete(Article article) {
+		repository.delete(article);
+	}
 }
