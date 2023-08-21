64a12c6abbb46096e13ba01c8c4bfe2576751dfc
Matthieu DAVID <mat.david39700@gmail.com>
2023-08-21 10:01:51 +0200

    First commit

---
+++ src/main/java/fr/tpwebshopmvc/TpwebshopmvcApplication.java
@@ -0,0 +1,13 @@
+package fr.tpwebshopmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpwebshopmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpwebshopmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tpwebshopmvc/TpwebshopmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tpwebshopmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpwebshopmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
