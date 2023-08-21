c798e550603fc23678b1f7e7960716040283b519
Matthieu DAVID <mat.david39700@gmail.com>
2023-08-21 14:15:27 +0200

    Ajout index + debut code articles crud

--- src/main/java/fr/tpwebshopmvc/controller/ArticleController.java
+++ src/main/java/fr/tpwebshopmvc/controller/ArticleController.java
@@ -1,8 +1,27 @@
 package fr.tpwebshopmvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.tpwebshopmvc.model.Article;
+import fr.tpwebshopmvc.service.ArticleService;
 
 @Controller
 public class ArticleController {
-
+	
+	@Autowired
+	private ArticleService articleService;
+	
+	@GetMapping("/articles")
+	public ModelAndView showArticles()
+	{
+		System.out.println("articles ");
+		List<Article> allArticles = articleService.findAll();
+		return new ModelAndView("articles","articles",allArticles);
+	}
+	
 }
