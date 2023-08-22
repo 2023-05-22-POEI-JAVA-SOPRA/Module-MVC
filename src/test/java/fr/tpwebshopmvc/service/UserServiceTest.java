1d2e91236a1be96baa23b1c6b0793940480a7a06
MaxCLZ <maxence.clusaz@gmail.com>
2023-08-22 11:08:30 +0200

    aide mathieu bloqué depuis 2h

--- src/test/java/fr/tpwebshopmvc/service/UserServiceTest.java
+++ src/test/java/fr/tpwebshopmvc/service/UserServiceTest.java
@@ -10,16 +10,22 @@ import fr.tpwebshopmvc.model.User;
 @SpringBootTest
 public class UserServiceTest {
 	
-//	@Autowired
-//	UserService userService;
-//	
-//	
-//	
-//	@Test
-//	public void test()
-//	{
+	@Autowired
+	UserService userService;
+	
+	
+	
+	@Test
+	public void test()
+	{
 //		Iterable<User> users = userService.getAll();
-//
-//	}
+		
+		User userInDb = userService.getById(1);
+		Assertions.assertTrue(userInDb.getIdUser() != null, "User in db not found");
+		userService.deleteById(1);
+		User userDeleted = userService.getById(1);
+		Assertions.assertNull(userDeleted );
+
+	}
 
 }
