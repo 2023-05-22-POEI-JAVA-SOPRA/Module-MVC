adc3076b12266da7e6ac13e43f060889cd6cc53d
Amandine Cian <amandine.cian@gmail.com>
2023-08-22 09:05:29 +0200

    retrait des anciennes méthodes et refactor en une seule méthode

--- src/main/java/groupe1/webshop/application/jpa/controller/UserController.java
+++ src/main/java/groupe1/webshop/application/jpa/controller/UserController.java
@@ -14,36 +14,12 @@ public class UserController {
 	@Autowired
 	private serviceUser sUser;
 	
-//		@GetMapping(path="/users")
-//		public String allUser() {
-//			System.out.println("Méthode sur /users appelée");
-//			return "user";
-//		}
-//		
-//		//Methode appelee lors du chargement de la page planetes.html
-//		@ModelAttribute("users")
-//		public Iterable<User> listUsers() {
-//			return sUser.getAll();
-//		}
-
-//		@GetMapping("/users/search")
-//		public ModelAndView searchUser(@ModelAttribute("users") PojoLogin login) {
-////			ModelAndView mav = new ModelAndView("user", "user", sUser.getByLoginContainingIgnoreCase(login));
-//			
-//			ModelAndView mav = new ModelAndView();
-//			mav.setViewName("user");
-//			mav.addObject("users", sUser.getByLoginContainingIgnoreCase(login.getLogin()));
-////			mav.setViewName("user" +  login.getLogin("login"));
-//			
-//			System.out.println("recherche par login");
-//			return mav;
-//		}
-
-		@GetMapping("/users")
-		public ModelAndView searchUser(@RequestParam(name="login", required= false) String login) {
-		    ModelAndView mav = new ModelAndView();
-		    mav.setViewName("user");
-		    mav.addObject("users", sUser.getByLoginContainingIgnoreCase(login));
-		    return mav;
-		}
+//méthode permettant de gérer à la fois l'affichage de toute la liste et la recherche par login
+	@GetMapping("/users")
+	public ModelAndView searchUser(@RequestParam(name="login", required= false) String login) {
+	    ModelAndView mav = new ModelAndView();
+	    mav.setViewName("user");
+	    mav.addObject("users", sUser.getByLoginContainingIgnoreCase(login));
+	    return mav;
+	}
 }
