ada57d0023625f3ce4acc9a24fffe96fba1a0f94
MaxCLZ <maxence.clusaz@gmail.com>
2023-08-22 10:56:05 +0200

    repair done

---
+++ src/test/java/fr/tpwebshopmvc/service/UserServiceTest.java
@@ -0,0 +1,25 @@
+package fr.tpwebshopmvc.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import fr.tpwebshopmvc.model.User;
+
+@SpringBootTest
+public class UserServiceTest {
+	
+//	@Autowired
+//	UserService userService;
+//	
+//	
+//	
+//	@Test
+//	public void test()
+//	{
+//		Iterable<User> users = userService.getAll();
+//
+//	}
+
+}
