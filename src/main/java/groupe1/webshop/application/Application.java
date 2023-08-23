9c2b3becdf889306561af71c4660d6c857641298
Laurent Plessis <Laurent.plessis@gmail.com>
2023-08-23 12:16:58 +0200

    ajouter javadoc

--- src/main/java/groupe1/webshop/application/Application.java
+++ src/main/java/groupe1/webshop/application/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
